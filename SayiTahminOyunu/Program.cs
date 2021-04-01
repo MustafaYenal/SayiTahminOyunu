39cf539563039b7bb7ea1338a66145377e5e9c93
MUSTAFA YENAL <202803018@ogr.cbu.edu.tr>
2021-04-01 20:53:16 +0300

    Add project files.

---
+++ SayiTahminOyunu/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SayiTahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tahmin=0;
+            int sayac = 0;
+            int hafiza;
+            Random rastgele = new Random();
+            hafiza = rastgele.Next(1, 100);
+            while(tahmin != hafiza)
+            {
+                sayac++;
+                Console.WriteLine("Sayi girin: ");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                if(hafiza>tahmin)
+                {
+                    Console.WriteLine("Lütfen daha büyük sayi giriniz !!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen daha küçük sayi giriniz !!!\n");
+                }
+            }
+            Console.WriteLine("Tebrikler...");
+            Console.WriteLine("{0}.denemede buldunuz", sayac);
+        }
+    }
+}
